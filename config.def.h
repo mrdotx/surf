854a85384eb9f40016eeabfe238895a66a19e6f3
mrdotx <klassiker@gmx.de>
2019-12-19 13:24:56 +0100

    add dlconsole

--- config.def.h
+++ config.def.h
@@ -7,6 +7,8 @@ static char *certdir        = "~/.local/share/surf/certificates/";
 static char *cachedir       = "~/.cache/surf/cache/";
 static char *cookiefile     = "~/.local/share/surf/cookies.txt";
 static char *searchurl      = "duckduckgo.com/?q=%s";
+static char *dldir          = "~/Downloads/";
+static char *dlstatus       = "~/.cache/surf/dlstatus/";
 
 /* Webkit default features */
 /* Highest priority value will be used.
@@ -86,12 +88,12 @@ static WebKitFindOptions findopts = WEBKIT_FIND_OPTIONS_CASE_INSENSITIVE |
 }
 
 /* DOWNLOAD(URI, referer) */
-#define DOWNLOAD(u, r) { \
+#define DLSTATUS { \
         .v = (const char *[]){ "st", "-e", "/bin/sh", "-c",\
-             "curl -g -L -J -O -A \"$1\" -b \"$2\" -c \"$2\"" \
-             " -e \"$3\" \"$4\"; read", \
-             "surf-download", useragent, cookiefile, r, u, NULL \
-        } \
+             "while true; do cat $1/* 2>/dev/null || echo \"no hay descargas\";"\
+             "A=; read A; "\
+             "if [ $A = \"clean\" ]; then rm $1/*; fi; clear; done",\
+             "surf-dlstatus", dlstatus, NULL } \
 }
 
 /* PLUMB(URI) */
@@ -190,6 +192,9 @@ static Key keys[] = {
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_b,      toggle,     { .i = ScrollBars } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_t,      toggle,     { .i = StrictTLS } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_m,      toggle,     { .i = Style } },
+
+	/* download-console */
+	{ MODKEY,                GDK_KEY_d,      spawndls,   { 0 } },
 };
 
 /* button definitions */
