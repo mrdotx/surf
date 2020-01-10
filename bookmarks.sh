2f0666b89653a897fdd641fe5667c779492d3428
mrdotx <klassiker@gmx.de>
2020-01-10 19:25:11 +0100

    changed to firefox

--- bookmarks.sh
+++ bookmarks.sh
@@ -3,9 +3,9 @@
 # path:       ~/projects/surf/bookmarks.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/surf
-# date:       2019-12-26 19:29:40
+# date:       2020-01-10 19:25:06
 
-# copy bookmarks from firefox developer edition
+# copy bookmarks from firefox
 echo 'select url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;' \
-    | sqlite3 ~/.mozilla/firefox/*.dev-edition-default/places.sqlite \
+    | sqlite3 ~/.mozilla/firefox/*.default-*/places.sqlite \
     | awk -F '//' '{print $2}' > ~/.config/surf/bookmarks
