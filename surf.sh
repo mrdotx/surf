02811bd4998737dfee2437f844bb1d7a78d43fae
mrdotx <klassiker@gmx.de>
2020-01-11 14:07:15 +0100

    surf tabbed changed

---
+++ surf.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# path:       ~/projects/surf/surf.sh
+# user:       klassiker [mrdotx]
+# github:     https://github.com/mrdotx/surf
+# date:       2020-01-11 13:51:09
+
+xidfile="/tmp/tabbed-surf.xid"
+uri=""
+
+if [ "$#" -gt 0 ];
+then
+    uri="$1"
+fi
+
+runtabbed() {
+    tabbed -cdn tabbed-surf -r 2 surf -e '' "$uri" >"$xidfile" 2>/dev/null &
+}
+
+if [ ! -r "$xidfile" ];
+then
+    runtabbed
+else
+    xid=$(cat "$xidfile")
+    if xprop -id "$xid" >/dev/null 2>&1;
+    then
+        surf -e "$xid" "$uri" >/dev/null 2>&1 &
+    else
+        runtabbed
+    fi
+fi
