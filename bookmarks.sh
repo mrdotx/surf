1f1ae2d80259e30190ef51570a824c8f828a2dd9
mrdotx <klassiker@gmx.de>
2020-01-12 22:34:14 +0100

    removed blank lines and sort the bookmarks

--- bookmarks.sh
+++ bookmarks.sh
@@ -3,9 +3,11 @@
 # path:       ~/projects/surf/bookmarks.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/surf
-# date:       2020-01-10 19:25:06
+# date:       2020-01-12 22:31:11
 
 # copy bookmarks from firefox
 echo 'select url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;' \
     | sqlite3 ~/.mozilla/firefox/*.default-*/places.sqlite \
-    | awk -F '//' '{print $2}' > ~/.config/surf/bookmarks
+    | awk -F '//' '{print $2}' \
+    | sed '/^$/d' \
+    | sort > ~/.config/surf/bookmarks
