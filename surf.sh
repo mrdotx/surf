c295af5e06ea3a546b6c36b0722c6f3046d096d7
mrdotx <klassiker@gmx.de>
2020-09-14 10:27:51 +0200

    useless condition removed

--- surf.sh
+++ surf.sh
@@ -3,15 +3,12 @@
 # path:       /home/klassiker/.local/share/repos/surf/surf.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/surf
-# date:       2020-09-14T00:02:30+0200
+# date:       2020-09-14T10:00:59+0200
 
 xidfile="/tmp/surf/tabbed-surf.xid"
-uri=""
+uri="$1"
 
 mkdir -p "/tmp/surf"
-
-[ "$#" -gt 0 ] \
-    && uri="$1"
 
 runtabbed() {
     tabbed -cdn tabbed-surf -r 2 surf -e '' "$uri" >"$xidfile" 2>/dev/null &
