efa82d4f084a49a3180d1bfa2008693bd328a944
mrdotx <klassiker@gmx.de>
2020-12-26 11:50:09 +0100

    delay increased for open multiple uri in tabbed

--- surf.sh
+++ surf.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/surf/surf.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/surf
-# date:       2020-12-26T11:37:57+0100
+# date:       2020-12-26T11:49:26+0100
 
 xidfile="/tmp/surf/tabbed-surf.xid"
 mkdir -p "/tmp/surf"
@@ -12,7 +12,7 @@ for uri in "$@"; do
 
 runtabbed() {
     tabbed -cdn tabbed-surf -r 2 surf -e '' "$uri" >"$xidfile" 2>/dev/null &
-    sleep .1
+    sleep .5
 }
 
 if [ ! -r "$xidfile" ];
